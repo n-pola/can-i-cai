77c7718a278c8f505015517220b5068a1ec06edd
Nils Polarek <n.polarek@live.de>
2024-06-04 14:51:21 +0200

    setup the frontend

---
+++ frontend/src/main.ts
@@ -0,0 +1,38 @@
+import './assets/main.css';
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { createI18n } from 'vue-i18n';
+import Toast, { POSITION, type PluginOptions } from 'vue-toastification';
+
+import '@/assets/styles/main.scss';
+import 'vue-toastification/dist/index.css';
+import englishLocales from '@/assets/locales/en.json';
+import germanLocales from '@/assets/locales/de.json';
+
+import App from './App.vue';
+import router from './router';
+
+const i18n = createI18n({
+  locale: 'en',
+  messages: {
+    en: englishLocales,
+    de: germanLocales,
+  },
+});
+
+const toastOptions: PluginOptions = {
+  position: POSITION.TOP_RIGHT,
+  timeout: 7000,
+  transition: 'Vue-Toastification__fade',
+  draggable: false,
+};
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(i18n);
+app.use(Toast, toastOptions);
+
+app.mount('#app');
