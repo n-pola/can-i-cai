063eb15aa330fa696974aafc75aece0c722ca7e2
Nils Polarek <n.polarek@live.de>
2024-06-09 00:01:46 +0200

    Add initial footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -34,6 +34,14 @@ const i18n = useI18n();
   <main>
     <RouterView />
   </main>
+
+  <footer>
+    <div class="footer-links">
+      <RouterLink to="/imprint" class="footer-legal">{{ i18n.t('imprint') }}</RouterLink>
+      <span> | </span>
+      <RouterLink to="/privacy" class="footer-legal">{{ i18n.t('privacyPolicy') }}</RouterLink>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss">
@@ -72,6 +80,30 @@ main {
   }
 
   &--active {
+    color: $primary;
+  }
+}
+
+footer {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  box-sizing: border-box;
+  display: flex;
+  flex-flow: row;
+  justify-content: flex-end;
+  width: 100%;
+}
+
+.footer-links {
+  display: flex;
+  gap: $xxs;
+  padding: $s $l;
+  background-color: $lightest;
+}
+
+.footer-legal {
+  &:hover {
     color: $primary;
   }
 }
